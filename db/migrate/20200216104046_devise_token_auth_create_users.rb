aca5056e0d50eed28103841fc3ffd77871802b03
Kanezoh <asfaltmanta114@gmail.com>
2020-02-16 20:45:07 +0900

    Fix Indent

--- db/migrate/20200216104046_devise_token_auth_create_users.rb
+++ db/migrate/20200216104046_devise_token_auth_create_users.rb
@@ -23,12 +23,12 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
-       ## Trackable 追記する
-       t.integer  :sign_in_count, :default => 0, :null => false
-       t.datetime :current_sign_in_at
-       t.datetime :last_sign_in_at
-       t.string   :current_sign_in_ip
-       t.string   :last_sign_in_ip
+      ## Trackable 追記する
+      t.integer  :sign_in_count, :default => 0, :null => false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
