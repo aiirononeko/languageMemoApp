2321568f04b242a588cab47d2c8f6e7c1a3d364b
show <show75@ezweb.ne.jp>
2020-02-21 17:51:47 +0900

    #11 ログインフォームを作成

---
+++ frontend/pages/user/login.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+        <v-card width="600px" class="mx-auto mt-5">
+            <v-card-text>
+                <v-form>
+                    <v-text-field  
+                    v-model="email"
+                    label="メール" />
+
+                    <v-text-field
+                    v-bind:rules="[rules.required, rules.min]"
+                    v-model="password"
+                    v-bind:type="showPassword ? 'text' : 'password'"
+                    v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    label="パスワード"
+                    counter
+                    @click:append="showPassword = !showPassword" />
+
+                    <v-card-actions>
+                         <v-btn class="info" large block>ログイン</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        showPassword: false,
+        password: "",
+        email: "",
+        rules: {
+          required: value => { return !!value || 'Required.'  },
+          min: value => { return value.length >= 8 || 'Min 8 characters'} ,
+        },
+    })
+};
+</script>
