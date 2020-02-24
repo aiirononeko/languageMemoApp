6c59196e0d6243719aa88986d1a820fb03615338
show <show75@ezweb.ne.jp>
2020-02-25 00:36:02 +0900

    #15 クライアントのヘッダでaccess-tokenを取得できるように設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3002' #ここにうんたら.comみたいな許可したいオリジンを指定する
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', :headers => :any, :methods => [:get, :post, :options], :expose => ['access-token']
   end
 end
