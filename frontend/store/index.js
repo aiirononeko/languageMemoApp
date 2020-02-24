56a111ce57b31d99b76d9aea3abee54a11e83180
show <show75@ezweb.ne.jp>
2020-02-25 00:43:18 +0900

    #15 ログインしてstoreにaccess-tokenを保存する処理を記述

---
+++ frontend/store/index.js
@@ -0,0 +1,36 @@
+
+import axios from 'axios'
+// const cookieparser = process.server ? require('cookieparser') : undefined
+
+export const state = () => {
+  return {
+    access_token: null,
+    uid: null
+  }
+}
+export const mutations = {
+  setUser (state, res) {
+    state.access_token = res.headers['access-token']
+    state.uid = res.data.data.uid
+    // state.client
+  }
+}
+
+export const actions = {
+    async login ({ commit }, { email, password }) {
+      try {
+        await axios.post('http://localhost:3000/api/v1/auth/sign_in', { email, password }
+        ).then(res => {
+            // console.log(res)
+            // console.log(res.data.data.uid)
+            commit('setUser', res)
+        })
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          throw new Error('Bad credentials')
+        }
+        throw error
+      }
+    },
+  
+  }
