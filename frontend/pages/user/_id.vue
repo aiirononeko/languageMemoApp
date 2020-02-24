7f66625aca452ff36a15ccf4b98701d2fcf876f3
show <show75@ezweb.ne.jp>
2020-02-25 00:45:22 +0900

    #15 静的マイページの作成

---
+++ frontend/pages/user/_id.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h1>マイページ</h1>
+        <p>{{ data }}</p>
+    </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    asyncData ({ app }) {
+      return app.$axios.get(`http://localhost:3000/api/v1/users/3`)
+      .then((res) => {
+        return { data: res.data }
+      })
+    }
+}
+</script>
