9e8877c8dd2a520da99e0df36914967130b95e91
show <show75@ezweb.ne.jp>
2020-02-25 13:48:41 +0900

    #15 使い方がわからなかったbaseURLの削除

--- frontend/pages/user/_id.vue
+++ frontend/pages/user/_id.vue
@@ -2,6 +2,10 @@
     <section>
         <h1>マイページ</h1>
         <p>{{ data }}</p>
+        <hr>
+        <h3>名前： {{ data.data.attributes.name }}</h3>
+        <h3>ユーザID： {{ $store.state.id }}</h3>
+        <nuxt-link to="/">Home</nuxt-link>
     </section>
 </template>
 
@@ -9,11 +13,11 @@
 import axios from 'axios'
 
 export default {
-    asyncData ({ app }) {
-      return app.$axios.get(`http://localhost:3000/api/v1/users/3`)
-      .then((res) => {
-        return { data: res.data }
-      })
+    asyncData({ $axios, params }) {
+        return $axios.$get(`http://localhost:3000/api/v1/users/${params.id}`)
+        .then((res) => {
+            return {data: res}
+        })
     }
 }
 </script>
