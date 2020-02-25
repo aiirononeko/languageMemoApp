1ac1ff2fb6fdc82a02a3e36f135f9e7eb25b1506
show <show75@ezweb.ne.jp>
2020-02-25 13:50:24 +0900

    #15 新規登録画面からユーザ作成ができるように

--- frontend/pages/user/sign_up.vue
+++ frontend/pages/user/sign_up.vue
@@ -16,11 +16,6 @@
 
                     <v-text-field  
                     v-bind:rules="[rules.required]"
-                    v-model="nickname"
-                    label="ニックネーム" />
-
-                    <v-text-field  
-                    v-bind:rules="[rules.required]"
                     v-model="email"
                     label="メール" />
 
@@ -33,17 +28,8 @@
                     counter
                     @click:append="showPassword = !showPassword" />
 
-                    <v-text-field
-                    v-bind:rules="[rules.required, rules.min]"
-                    v-model="passwordConfirmation"
-                    v-bind:type="showPassword ? 'text' : 'password'"
-                    v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                    label="パスワード確認"
-                    counter
-                    @click:append="showPassword = !showPassword" />
-
                     <v-card-actions>
-                         <v-btn v-on:click="login" class="info" large block>ログイン</v-btn>
+                         <v-btn v-on:click="signUp" class="info" large block>ログイン</v-btn>
                     </v-card-actions>
                 </v-form>
             </v-card-text>
@@ -52,31 +38,27 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data: () => ({
         showPassword: false,
         name: "",
-        nickname: "",
         email: "",
         password: "",
-        passwordConfirmation: "",
         rules: {
           required: value => { return !!value || 'Required.'  },
           min: value => { return value.length >= 8 || 'Min 8 characters'} ,
         },
     }),
     methods: {
-        async login() {
-            try {
-                await this.$auth.loginWith('local', {
-                    data: {
-                            email: this.email,
-                            password: this.password
-                    }
-                })
-            } catch (e) {
-                window.console.log(e)
-            }
+        async signUp() {
+            axios.post('http://localhost:3000/api/v1/auth', {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            })
+            this.$router.push(`/user/confirm`)
         }
   }
 };
