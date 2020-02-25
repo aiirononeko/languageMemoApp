ec49d5e0e737e352cfccf6e93d1b7f18be60fb6d
show <show75@ezweb.ne.jp>
2020-02-25 13:51:01 +0900

    #15 メール確認画面の作成

---
+++ frontend/pages/user/confirm.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <h1>メールをご確認ください</h1>
+        <a href="http://localhost:3000/letter_opener" target="_blank">メールを確認</a><br>
+        <nuxt-link to="/">Home</nuxt-link><br>
+        <nuxt-link to="/user/login">ログイン画面へ</nuxt-link>
+    </section>
+</template>
