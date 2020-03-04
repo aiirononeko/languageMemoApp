f00d577f84ff9bf01aca45262eeb0aa1b1272df4
Kanezoh <asfaltmanta114@gmail.com>
2020-03-04 10:43:01 +0900

    Userテーブルにaddressとprofileカラムを追加

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :nickname, :image, :confirmed_at, :updated_at
+  attributes :id, :name, :nickname, :image, :profile, :address, :confirmed_at, :updated_at
 end
