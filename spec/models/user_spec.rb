e5ad0e09d46e1a76852c9cae8cfb9e46d1d7167a
Kanezoh <asfaltmanta114@gmail.com>
2020-03-17 17:33:06 +0900

    usersカラムのデータ型、制約を一部変更、rspec追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,15 @@ RSpec.describe User, type: :model do
         expect(user.errors.messages[:password]).to include "can't be blank"
       end
     end
+
+    context "nameを指定していないとき" do
+      let(:user) { build(:user, name: nil) }
+
+      it "エラーになる" do
+        user.valid?
+        expect(user.errors.messages[:name]).to include "can't be blank"
+      end
+    end
   end
 
   describe "validates uniqueness" do
@@ -44,10 +53,23 @@ RSpec.describe User, type: :model do
   describe "validates length" do
     context "パスワードが８文字以下の場合" do
       let(:user) { build(:user, password: 'aaaa') }
-
       it "エラーになる" do
         user.valid?
         expect(user.errors.messages[:password]).to include "is too short (minimum is 8 characters)"
+      end
+    end
+    context "名前が50文字以上の場合" do
+      let(:user) { build(:user, name: 'a' * 51) }
+      it "エラーになる" do
+        user.valid?
+        expect(user.errors.messages[:name]).to include "is too long (maximum is 50 characters)"
+      end
+    end
+    context "住所が30文字以上の場合" do
+      let(:user) { build(:user, address: 'a' * 31) }
+      it "エラーになる" do
+        user.valid?
+        expect(user.errors.messages[:address]).to include "is too long (maximum is 30 characters)"
       end
     end
   end
