a0d5ac8b7f186c4e821a1eab25f0b954606e18a1
Kanezoh <asfaltmanta114@gmail.com>
2020-03-17 17:33:06 +0900

    usersカラムのデータ型、制約を一部変更、rspec追加

--- db/migrate/20200304013410_add_profile_and_address_to_users.rb
+++ db/migrate/20200304013410_add_profile_and_address_to_users.rb
@@ -1,11 +1,11 @@
 class AddProfileAndAddressToUsers < ActiveRecord::Migration[5.2]
   def up
-    add_column :users, :address, :text
+    add_column :users, :address, :string, limit: 30
     add_column :users, :profile, :string
   end
 
   def down
-    remove_column :users, :address, :text
+    remove_column :users, :address, :string
     remove_column :users, :profile, :string
   end
 end
