aedaf90ecdd36d397d63d805d901144697d62543
H37kouya <h37kouya@gmail.com>
2020-03-24 17:45:44 +0900

    delete node_modules && .nuxt

--- frontend/.nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['authenticated'] = require('../middleware/authenticated.js')
-middleware['authenticated'] = middleware['authenticated'].default || middleware['authenticated']
-
-middleware['clearNotification'] = require('../middleware/clearNotification.js')
-middleware['clearNotification'] = middleware['clearNotification'].default || middleware['clearNotification']
-
-export default middleware
