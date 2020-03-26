df4c9bbf70860d5e4959aad51925b0330527741d
show <show75@ezweb.ne.jp>
2020-03-26 18:32:26 +0900

    #64 onRequestを使用

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,5 +1,7 @@
 export default function({ app: { $axios }, store }) {
-  $axios.setHeader("access-token", store.state.access_token);
-  $axios.setHeader("uid", store.state.uid);
-  $axios.setHeader("client", store.state.client);
+  $axios.onRequest(config => {
+    $axios.setHeader("access-token", store.state.access_token);
+    $axios.setHeader("uid", store.state.uid);
+    $axios.setHeader("client", store.state.client);
+  })
 }
