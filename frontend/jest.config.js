1238ca98e9c817049620269ea1932fa86eee10da
h37kouya <h37kouya@gmail.com>
2020-03-28 14:35:40 +0900

    #63 jestのセットアップ

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
-  },
+  setupFiles: ['<rootDir>/tests/jest.setup.js'],    // Load Mock setting
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1'
   },
-  moduleFileExtensions: ['js', 'json', 'vue']
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
 }
