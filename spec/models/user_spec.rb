616829eb94252585c1d4d09d4666bdd7c7e3b1fb
Kanezoh <asfaltmanta114@gmail.com>
2020-04-06 00:23:09 +0900

    パスワード、ユーザーネームに半角英数字かアンダーバーのバリデーションを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -81,4 +81,36 @@ RSpec.describe User, type: :model do
       end
     end
   end
+  describe "validates regular expression" do
+    context "パスワードが半角英数字とアンダーバーのみの場合" do
+      let(:user) { build(:user, password: 'aaa_aaaa') }
+      it "正常に保存できる" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "パスワードが半角英数字とアンダーバー以外を含む場合" do
+      let(:user) { build(:user, password: 'a' * 7 + 'あ') }
+      it "エラーになる" do
+        user.valid?
+        expect(user.errors.messages[:password]).to include "is invalid"
+      end
+    end
+
+    context "ユーザーネームが半角英数字とアンダーバーのみの場合" do
+      let(:user) { create(:user) }
+      it "正常に更新できる" do
+        user.update(username: 'aaa_aaaa')
+        expect(user).to be_valid
+      end
+    end
+
+    context "ユーザーネームが半角英数字とアンダーバー以外を含む場合" do
+      let(:user) { create(:user) }
+      it "正常に更新できない" do
+        user.update(username: 'aaaあaaaa')
+        expect(user.errors.messages[:username]).to include "is invalid"
+      end
+    end
+  end
 end
