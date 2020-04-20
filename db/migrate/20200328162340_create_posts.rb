673240b763eb14c6d3f4bb7952bfa9b89c60904d
榑林晃平 <kureykou321@gmail.com>
2020-04-21 05:29:13 +0900

    publicのdefaultをtrueに変更とschemaを更新

--- db/migrate/20200328162340_create_posts.rb
+++ db/migrate/20200328162340_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true, null: false, index: true
       t.string :name, null: false
       t.text :content, null: false
-      t.boolean :public, null: false, default: false, comment: "公開or非公開"
+      t.boolean :public, null: false, default: true, comment: "公開or非公開"
       t.timestamps
     end
   end
