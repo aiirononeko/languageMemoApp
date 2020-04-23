1f3cc8d87771b772fa6594a06713d06dca969ad0
H37kouya <h37kouya@gmail.com>
2020-04-23 22:45:14 +0900

    pathを処理する関数の追加

---
+++ frontend/utils/path.js
@@ -0,0 +1,67 @@
+/**
+ * @see https://nodejs.org/docs/latest-v10.x/api/path.html
+ */
+import { dirname, extname, basename } from 'path'
+
+/**
+ * ディレクトリの配列を取得する
+ *
+ * @param { String } p パス
+ * @returns { String[] }
+ */
+export const getDirArr = (p) => {
+  p = p.startsWith("/") ? p.slice(1) : p
+  return dirname(p).split("/");
+}
+
+/**
+ * 拡張子を取得する
+ * .gitignoreのような.から始まるファイル名はnullになる。
+ *
+ * @param { String } p パス
+ * @returns { String|null }
+ * @example .cpp .js .ts .php
+ */
+export const getExt = (p) => extname(basename(p)) || null
+
+/**
+ * ディレクトリ名を取得する
+ *
+ * @param { String } p パス
+ * @returns { String|null }
+ */
+export const getDirname = (p) => isDir(p) ? getLastname(p) : null
+
+/**
+ * ファイル名を取得する
+ *
+ * @param { String } p パス
+ * @returns { String }
+ * @example index.html index.php dist.js .gitignore
+ */
+export const getFilename = (p) => isFile(p) ? getLastname(p) : null
+
+/**
+ * pathの最後の部分を取得する
+ *
+ * @param { String } p パス
+ * @returns { String }
+ * @example index.html index.php dist.js .gitignore dir hogehoge
+ */
+export const getLastname = (p) => basename(p)
+
+/**
+ * フォルダであるか
+ *
+ * @param { String } p パス
+ * @returns { Boolean }
+ */
+export const isDir = (p) => !isFile(p)
+
+/**
+ * ファイル名であるか
+ *
+ * @param { String } p パス
+ * @returns { Boolean }
+ */
+export const isFile = (p) => basename(p).includes('.')
