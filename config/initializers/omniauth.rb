961415111ba4d2f1496f5ee39db93ff940cc8611
Kanezoh <asfaltmanta114@gmail.com>
2020-04-23 17:38:19 +0900

    Twitter認証を作成

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile'
-  # provider :twitter,      ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter,       ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET']
 end
