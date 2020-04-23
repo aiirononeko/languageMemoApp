c4122fdd542ee125bfa90f9bf070b697e3e21910
H37kouya <h37kouya@gmail.com>
2020-04-24 00:36:44 +0900

    SNSアイコンの追加

---
+++ frontend/components/molecules/btns/GithubBtn.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-btn :dark="dark" :href="href" :to="to" @click="onClick">
+    <github-icon />
+  </v-btn>
+</template>
+
+<script>
+import GithubIcon from '~/components/atoms/icons/GithubIcon'
+
+export default {
+  components: {
+    GithubIcon
+  },
+
+  props: {
+    dark: {
+      type: Boolean,
+      default: true
+    },
+
+    to: {
+      type: [String, Object],
+      default: undefined
+    },
+
+    href: {
+      type: String,
+      default: undefined
+    }
+  },
+
+  methods: {
+    onClick() {
+      return this.$emit('click')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
