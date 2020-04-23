b4c4a2cc26148ee588476200cb80ab34c555cb80
H37kouya <h37kouya@gmail.com>
2020-04-23 22:45:55 +0900

    routingの追加

---
+++ frontend/pages/edit/_fileID.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>ファイル名：{{ fileID }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: "authenticated",
+
+  computed: {
+    fileID() {
+      return this.$route.params.fileID
+    },
+
+    status() {
+      return this.$route.query.status
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
