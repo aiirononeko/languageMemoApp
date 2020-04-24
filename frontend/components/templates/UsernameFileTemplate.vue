3ef565fa4a14d50b60abbbe7fb9a47506084da22
H37kouya <h37kouya@gmail.com>
2020-04-24 17:45:46 +0900

    ファイル操作系のものを追加

--- frontend/components/templates/UsernameFileTemplate.vue
+++ frontend/components/templates/UsernameFileTemplate.vue
@@ -1,19 +1,28 @@
 <template>
-  <two-column-container>
+  <two-column-container :leftCols="8" :rightCols="4">
     <template #left>
     </template>
 
     <template #right>
+      <user-intro-card />
+
+      <file-folder-list />
     </template>
   </two-column-container>
 </template>
 
 <script>
+import FileFolderList from '~/components/organisms/list/FileFolderList'
+import FolderBreadcrumbs from '~/components/organisms/breadcrumbs/FolderBreadcrumbs'
 import TwoColumnContainer from '~/components/molecules/containers/TwoColumnContainer'
+import UserIntroCard from '~/components/organisms/cards/UserIntroCard'
 
 export default {
   components: {
-    TwoColumnContainer
+    FileFolderList,
+    FolderBreadcrumbs,
+    TwoColumnContainer,
+    UserIntroCard
   }
 }
 </script>
