02acda2c1d25a7f92cbf04e060a651b8e692e427
H37kouya <h37kouya@gmail.com>
2020-04-24 12:41:14 +0900

    containerの追加

---
+++ frontend/components/molecules/containers/OneColumnContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-container>
+    <v-row justify="center">
+      <v-col>
+        <slot />
+      </v-col>
+    </v-row>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from '~/components/organisms/containers/BaseContainer'
+
+export default {
+  components: {
+    BaseContainer
+  }
+}
+</script>
+
+<style>
+
+</style>
