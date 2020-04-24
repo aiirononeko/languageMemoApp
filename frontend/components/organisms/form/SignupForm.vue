c1c3b10590d7d41c980b3d8e3d9502a65762d251
H37kouya <h37kouya@gmail.com>
2020-04-24 15:47:27 +0900

    signupの追加

--- frontend/components/organisms/form/SignupForm.vue
+++ frontend/components/organisms/form/SignupForm.vue
@@ -1,15 +1,22 @@
 <template>
   <v-form>
-    <username-text-field />
+    <email-text-field />
+    <password-text-field />
+
+    <orange-btn type="submit" />
   </v-form>
 </template>
 
 <script>
-import UsernameTextField from '~/components/organisms/textFields/UsernameTextField'
+import EmailTextField from '~/components/organisms/textFields/EmailTextField'
+import PasswordTextField from '~/components/organisms/textFields/PasswordTextField'
+import OrangeBtn from '~/components/atoms/btns/OrangeBtn'
 
 export default {
   components: {
-    UsernameTextField
+    EmailTextField,
+    PasswordTextField,
+    OrangeBtn
   }
 }
 </script>
