2050272ee45744a69a2afccf845922438a011aa1
H37kouya <h37kouya@gmail.com>
2020-04-24 15:43:54 +0900

    プロフィール編集を追加

---
+++ frontend/components/organisms/form/EditProfileForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-form>
+  </v-form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/components/templates/SettingProfileTemplate.vue
+++ frontend/components/templates/SettingProfileTemplate.vue
@@ -1,14 +1,19 @@
 <template>
   <one-column-container>
+    <h1>プロフィール編集</h1>
+
+    <setting-profile-card />
   </one-column-container>
 </template>
 
 <script>
 import OneColumnContainer from '~/components/molecules/containers/OneColumnContainer'
+import SettingProfileCard from '~/components/organisms/cards/SettingProfileCard'
 
 export default {
   components: {
-    OneColumnContainer
+    OneColumnContainer,
+    SettingProfileCard
   }
 }
 </script>
