2b799a767d7df94112fc3f15c02d97bc5f05b306
H37kouya <h37kouya@gmail.com>
2020-04-24 18:23:02 +0900

    markdown系のコンポーネントを追加

--- frontend/components/templates/UsernameFileTemplate.vue
+++ frontend/components/templates/UsernameFileTemplate.vue
@@ -1,6 +1,7 @@
 <template>
   <two-column-container :leftCols="8" :rightCols="4">
     <template #left>
+      <view-file-card />
     </template>
 
     <template #right>
@@ -16,13 +17,15 @@ import FileFolderList from '~/components/organisms/list/FileFolderList'
 import FolderBreadcrumbs from '~/components/organisms/breadcrumbs/FolderBreadcrumbs'
 import TwoColumnContainer from '~/components/molecules/containers/TwoColumnContainer'
 import UserIntroCard from '~/components/organisms/cards/UserIntroCard'
+import ViewFileCard from '~/components/organisms/cards/ViewFileCard'
 
 export default {
   components: {
     FileFolderList,
     FolderBreadcrumbs,
     TwoColumnContainer,
-    UserIntroCard
+    UserIntroCard,
+    ViewFileCard
   }
 }
 </script>
