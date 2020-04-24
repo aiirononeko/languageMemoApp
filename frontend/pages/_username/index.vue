b41e419ebd6650e848d55a7f8b64d68419fa13b9
H37kouya <h37kouya@gmail.com>
2020-04-24 18:36:32 +0900

    pageとtemplateをつなげた

--- frontend/pages/_username/index.vue
+++ frontend/pages/_username/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <p>ユーザー名：{{ username }}</p>
+
+    <username-index-template />
   </div>
 </template>
 
 <script>
+import UsernameIndexTemplate from '~/components/templates/UsernameIndexTemplate'
+
 export default {
+  components: {
+    UsernameIndexTemplate
+  },
+
   computed: {
     username() {
       return this.$route.params.username
