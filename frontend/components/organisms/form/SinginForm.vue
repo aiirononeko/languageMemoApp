bd5de2bd0e80356fbbc911b3ebd13013d5cf5f7b
H37kouya <h37kouya@gmail.com>
2020-04-24 15:30:25 +0900

    loginの追加

---
+++ frontend/components/organisms/form/SinginForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form>
+    <email-text-field />
+    <password-text-field />
+
+    <orange-btn type="submit">
+      ログイン
+    </orange-btn>
+  </v-form>
+</template>
+
+<script>
+import EmailTextField from '~/components/organisms/textFields/EmailTextField'
+import PasswordTextField from '~/components/organisms/textFields/PasswordTextField'
+import OrangeBtn from '~/components/atoms/btns/OrangeBtn'
+
+export default {
+  components: {
+    EmailTextField,
+    PasswordTextField,
+    OrangeBtn
+  }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/components/templates/SigninTemplate.vue
+++ frontend/components/templates/SigninTemplate.vue
@@ -1,14 +1,19 @@
 <template>
   <one-column-container>
+    <h1>ログイン</h1>
+
+    <signin-card />
   </one-column-container>
 </template>
 
 <script>
 import OneColumnContainer from '~/components/molecules/containers/OneColumnContainer'
+import SigninCard from '~/components/organisms/cards/SigninCard'
 
 export default {
   components: {
-    OneColumnContainer
+    OneColumnContainer,
+    SigninCard
   }
 }
 </script>
