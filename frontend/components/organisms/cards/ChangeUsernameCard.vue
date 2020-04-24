ac3884358c5af570b6a43aec4274e9bd64fe67a9
H37kouya <h37kouya@gmail.com>
2020-04-24 15:36:35 +0900

    ユーザーネームを変えるを追加

---
+++ frontend/components/organisms/cards/ChangeUsernameCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    ユーザーIDを入力してください
+
+    <change-username-form />
+  </v-card>
+</template>
+
+<script>
+import ChangeUsernameForm from '~/components/organisms/form/ChangeUsernameForm'
+
+export default {
+  components: {
+    ChangeUsernameForm
+  }
+}
+</script>
+
+<style>
+
+</style>
