0c317808c836c59cb26c7774905ed96ed76270fe
H37kouya <h37kouya@gmail.com>
2020-04-24 12:40:48 +0900

    containerの場所を変えた

---
+++ frontend/components/templates/SigninTemplate.vue
@@ -0,0 +1,18 @@
+<template>
+  <one-column-container>
+  </one-column-container>
+</template>
+
+<script>
+import OneColumnContainer from '~/components/molecules/containers/OneColumnContainer'
+
+export default {
+  components: {
+    OneColumnContainer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/components/templates/UsernameFolderTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <two-column-container>
+    <template #left>
+    </template>
+
+    <template #right>
+    </template>
+  </two-column-container>
+</template>
+
+<script>
+import TwoColumnContainer from '~/components/molecules/containers/TwoColumnContainer'
+
+export default {
+  components: {
+    TwoColumnContainer
+  }
+}
+</script>
+
+<style>
+
+</style>
