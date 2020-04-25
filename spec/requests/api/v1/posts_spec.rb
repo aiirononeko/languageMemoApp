ceaf4171fab2b08accfe1c63954a84dd6919e0d6
榑林晃平 <kureykou321@gmail.com>
2020-04-25 09:22:58 +0900

    APIのテストだけ実行

---
+++ spec/requests/api/v1/posts_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Posts", type: :request do
+  describe "Post"do
+    subject(:call_api){}
+  end
+
+  describe "PATCH" do
+    subject(:call_api){}
+  end
+
+  describe "DELETE" do
+    subject(:call_api){}
+  end
+
+end
