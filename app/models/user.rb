aeec92233af86e0010331630ce10c29f362b1b4d
榑林晃平 <kureykou321@gmail.com>
2020-04-25 09:28:33 +0900

    ビジネスロジックの配置の整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
+  has_many :posts, dependent: :destroy
+
   VALID_USERNAME_REGEX = /\A[\w_]+\z/i
   VALID_PASSWORD_REGEX = /\A[!-~]+\z/
 
   validates :name, length: { maximum: 50 }
   validates :address, length: { maximum: 30 }
-
-  has_many :posts, dependent: :destroy
-
   validates :username, length: { maximum: 30 }, uniqueness: true, allow_nil: true, presence: true, format: { with: VALID_USERNAME_REGEX }
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
 end
