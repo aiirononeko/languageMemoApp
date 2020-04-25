e9d5a58eea654204cc16b00f5746a34e4b7304f5
show <show75@ezweb.ne.jp>
2020-04-25 16:32:25 +0900

    #58 eslintの設定

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,12 +10,22 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/essential',
+    'prettier'
   ],
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    "semi": [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+      }
+    }],
+  }
 }
