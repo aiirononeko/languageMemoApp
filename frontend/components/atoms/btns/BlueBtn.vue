293854f7e3592c7d6434969d75f57fc090d1a304
show <show75@ezweb.ne.jp>
2020-04-26 19:20:09 +0900

    #89 atoms/btnsを作成

---
+++ frontend/components/atoms/btns/BlueBtn.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-btn depressed color="blue accent-4" class="btn">
+        <slot/>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .btn {
+        color: white;
+    }
+</style>
---
+++ frontend/components/atoms/btns/OrangeBtn.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-btn depressed color="yellow lighten-3" class="btn">
+        <slot/>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .btn {
+        color: #747474;
+    }
+</style>
