3e9e82199b6c2bcb468efd77b524ce7a475e40d1
show <show75@ezweb.ne.jp>
2020-04-26 19:20:09 +0900

    #89 atoms/btnsを作成

---
+++ frontend/components/atoms/btns/RedBtn.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-btn depressed color="red lighten-2" class="btn">
+        <slot/>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .btn {
+        color: white;
+    }
+</style>
