87bc6b6f77e21d137909a73dcfbfcc3cc1fc7674
show <show75@ezweb.ne.jp>
2020-04-29 15:43:43 +0900

    #89 btnにtoを設定

--- frontend/components/atoms/btns/BlueBtn.vue
+++ frontend/components/atoms/btns/BlueBtn.vue
@@ -1,12 +1,17 @@
 <template>
-    <v-btn depressed color="blue accent-4" class="btn">
+    <v-btn depressed color="blue accent-4" class="btn" to="to">
         <slot/>
     </v-btn>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        to: {
+            type: String,
+            default: "#"
+        }
+    }
 }
 </script>
 
