8923cb91108653de839b0569fc18925a0ac3c131
show <show75@ezweb.ne.jp>
2020-04-29 15:43:43 +0900

    #89 btnにtoを設定

--- frontend/components/atoms/btns/OrangeBtn.vue
+++ frontend/components/atoms/btns/OrangeBtn.vue
@@ -1,12 +1,17 @@
 <template>
-    <v-btn depressed color="yellow lighten-3" class="btn">
+    <v-btn depressed color="yellow lighten-3" class="btn" to="to">
         <slot/>
     </v-btn>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        to: {
+            type: String,
+            default: "#"
+        }
+    }
 }
 </script>
 
--- frontend/components/atoms/btns/RedBtn.vue
+++ frontend/components/atoms/btns/RedBtn.vue
@@ -1,12 +1,17 @@
 <template>
-    <v-btn depressed color="red lighten-2" class="btn">
+    <v-btn depressed color="red lighten-2" class="btn" to="to">
         <slot/>
     </v-btn>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        to: {
+            type: String,
+            default: "#"
+        }
+    }
 }
 </script>
 
