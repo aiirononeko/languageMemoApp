1b626a69b36140656e9b59dfff5ccf7435afb374
show <show75@ezweb.ne.jp>
2020-04-29 20:20:04 +0900

    #89 UserSnsBtnGroupの作成

--- frontend/components/organisms/btnGroup/UserSnsBtnGroup.vue
+++ frontend/components/organisms/btnGroup/UserSnsBtnGroup.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
     <!-- 各ユーザーのsnsへのリンク -->
+    <twitter-btn fab :v-if="twitterLinkIsExists"/>
+    <github-btn fab :v-if="githubLinkIsExists"/>
   </div>
 </template>
 
 <script>
-export default {
+const TwitterBtn = () => import('~/components/molecules/btns/TwitterBtn')
+const GithubBtn = () => import('~/components/molecules/btns/GithubBtn')
 
+export default {
+  components: {
+    TwitterBtn,
+    GithubBtn
+  },
+
+  computed: {
+    twitterLinkIsExists() {
+      // TODO(Ropital): Twitterリンクが存在したらtrueを返す
+      return false
+    },
+    githubLinkIsExists() {
+      // TODO(Ropital): Githubリンクが存在したらtrueを返す
+      return false
+    },
+  }
 }
 </script>
 
 <style>
-
 </style>
