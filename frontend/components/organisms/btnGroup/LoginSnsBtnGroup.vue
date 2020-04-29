d695232750de9302a8d0555e102e56d303e62bb2
show <show75@ezweb.ne.jp>
2020-04-29 16:57:21 +0900

    #97 LoginSnSBtnGroupの作成

--- frontend/components/organisms/btnGroup/LoginSnsBtnGroup.vue
+++ frontend/components/organisms/btnGroup/LoginSnsBtnGroup.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <!-- snsでログインのボタンをまとめる -->
+    <twitter-btn>Twitter</twitter-btn> 
+    <google-btn>Google</google-btn>
+    <github-btn>Github</github-btn>
   </div>
 </template>
 
 <script>
-export default {
+import TwitterBtn from '~/components/molecules/btns/TwitterBtn'
+import GoogleBtn from '~/components/molecules/btns/GoogleBtn'
+import GithubBtn from '~/components/molecules/btns/GithubBtn'
 
+export default {
+  components: {
+    TwitterBtn,
+    GoogleBtn,
+    GithubBtn
+  }
 }
 </script>
 
