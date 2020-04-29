2591510b782896721ba93f6ebea06b9371b14f15
Kanezoh <asfaltmanta114@gmail.com>
2020-04-29 14:27:43 +0900

    デフォルトでランダムなユーザーネームを設定するようにする

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     render json: current_api_v1_user, serializer: UserSerializer
   end
 
-
   private
+
   def sign_up_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :username)
   end
 
   def account_update_params
@@ -21,5 +21,21 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
 
   def render_update_success
     render json: @resource, serializer: UserSerializer
+  end
+
+  def set_default_username
+    default_username = "user" + SecureRandom.alphanumeric(15)
+    @resource.update
+  end
+
+  def sign_up_params
+    params.permit(*params_for_resource(:sign_up)).merge(username: set_default_username)
+  end
+
+  def set_default_username
+    loop do 
+      default_username = SecureRandom.alphanumeric(15)
+      return default_username unless User.find_by(username: default_username)
+    end
   end
 end
