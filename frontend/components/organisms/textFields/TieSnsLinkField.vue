ba42a9eab7b370ea52e6b6d8a7ab0bbcc477e0bf
show <show75@ezweb.ne.jp>
2020-05-03 10:58:38 +0900

    SNS紐付けボタンからリンクに変更

---
+++ frontend/components/organisms/textFields/TieSnsLinkField.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Twitterのマイページリンク</p>
+    <v-text-field outlined dense />
+
+    <p>Githubのマイページリンク</p>
+    <v-text-field outlined dense />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
