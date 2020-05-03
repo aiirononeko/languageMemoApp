db9d83d6af99a1566be5d4bf99d48b8837eaec3e
show <show75@ezweb.ne.jp>
2020-05-03 11:08:34 +0900

    SNSLinkにモデル追加

--- frontend/components/organisms/textFields/TieSnsLinkField.vue
+++ frontend/components/organisms/textFields/TieSnsLinkField.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
     <p>Twitterのマイページリンク</p>
-    <v-text-field outlined dense />
+    <v-text-field v-model="twitterLink" outlined dense />
 
     <p>Githubのマイページリンク</p>
-    <v-text-field outlined dense />
+    <v-text-field v-model="githubLink" outlined dense />
   </div>
 </template>
 
 <script>
 export default {
+    data: () => {
+        return {
+            twitterLink: undefined,
+            githubLink: undefined
+        }
+    },
 
+    created() {
+        // TODO(Ropital): 初期値を入れる
+    }
 }
 </script>
 
