598ce9a862eb90c407be4e62113bc0f3d55353e2
show <show75@ezweb.ne.jp>
2020-05-05 14:58:46 +0900

    /signupの作成

--- frontend/pages/signup/index.vue
+++ frontend/pages/signup/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <signup-template />
+    <signup-template @signup="signup" />
   </div>
 </template>
 
@@ -10,6 +10,22 @@ import SignupTemplate from '~/components/templates/SignupTemplate'
 export default {
   components: {
     SignupTemplate
+  },
+
+  methods: {
+    async signup(userInfo) {
+      try {
+        console.log("singup")
+        await this.$axios.post(`/api/v1/auth`, {
+          email: userInfo.email,
+          password: userInfo.password
+        })
+
+        this.$router.push(`/user/confirm`)
+      } catch (error) {
+        console.error(error)
+      }
+    },
   }
 }
 </script>
