fcc1c2c2ddbcb7b7322ecfed28ae84471b4eb3c7
H37kouya <h37kouya@gmail.com>
2020-05-05 20:47:11 +0900

    srcをcoverage範囲に追加

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  setupFiles: ['<rootDir>/tests/jest.setup.js'],    // Load Mock setting
+  setupFiles: ["<rootDir>/tests/jest.setup.js"], // Load Mock setting
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/store/**/*.js',
-    '<rootDir>/utils/**/*.js',
+    "<rootDir>/src/**/*.js",
+    "<rootDir>/store/**/*.js",
+    "<rootDir>/utils/**/*.js",
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1'
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
   },
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ["js", "json", "vue"],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest",
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/']
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 }
