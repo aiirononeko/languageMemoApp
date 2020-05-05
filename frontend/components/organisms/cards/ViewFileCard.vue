53f18bb90bc47f47e8da16411edb467785f5ee6c
H37kouya <h37kouya@gmail.com>
2020-05-05 10:31:12 +0900

    Viewでマークダウンを変換できるようにした

--- frontend/components/organisms/cards/ViewFileCard.vue
+++ frontend/components/organisms/cards/ViewFileCard.vue
@@ -2,12 +2,24 @@
   <div>
     <!-- TODO: fileの内容を表示したい -->
     <!-- Ex: CSSの基礎、セレクタとは -->
+    <div id="md_view" v-html="html" />
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    md: {
+      type: String,
+      default: undefined
+    }
+  },
 
+  computed: {
+    html() {
+      return this.$md.render(this.md)
+    }
+  },
 }
 </script>
 
