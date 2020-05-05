a96e173054032a5689d686b5748496e382a1dbbc
H37kouya <h37kouya@gmail.com>
2020-05-05 14:59:56 +0900

    OneColumnContainerの付与とエラーを消した

--- frontend/components/templates/EditFileidSViewTemplate.vue
+++ frontend/components/templates/EditFileidSViewTemplate.vue
@@ -1,13 +1,17 @@
 <template>
-  <view-file-card :md="md" />
+  <one-column-container>
+    <view-file-card :md="md" />
+  </one-column-container>
 </template>
 
 <script>
 const ViewFileCard = () => import('~/components/organisms/cards/ViewFileCard')
+const OneColumnContainer = () => import('~/components/molecules/containers/OneColumnContainer')
 
 export default {
   components: {
-    ViewFileCard
+    ViewFileCard,
+    OneColumnContainer
   },
 
   props: {
