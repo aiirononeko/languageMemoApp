bf9118182fa268c850551f9906867759d3c618b4
show <show75@ezweb.ne.jp>
2020-05-05 21:47:45 +0900

    text-centerを使用

--- frontend/components/templates/SignupTemplate.vue
+++ frontend/components/templates/SignupTemplate.vue
@@ -1,6 +1,6 @@
 <template>
   <one-column-container>
-    <h1 class="mb-7 m-title">新規登録</h1>
+    <h1 class="mb-7 text-center">新規登録</h1>
 
     <signup-card @signup="signup" />
   </one-column-container>
@@ -25,7 +25,4 @@ export default {
 </script>
 
 <style>
-.m-title {
-  text-align: center;
-}
 </style>
