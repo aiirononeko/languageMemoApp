04ea97165b98958222067ea93321a9e912faf1e3
show <show75@ezweb.ne.jp>
2020-05-05 09:46:07 +0900

    asyncdataで取得したデータを表示

--- frontend/components/organisms/cards/SettingProfileCard.vue
+++ frontend/components/organisms/cards/SettingProfileCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card width="800" class="mx-auto pt-10 pl-6">
-      <edit-profile-form />
+      <edit-profile-form :info="info"/>
   </v-card>
 </template>
 
@@ -8,6 +8,13 @@
 const EditProfileForm = () => import('~/components/organisms/form/EditProfileForm')
 
 export default {
+  props: {
+    info: {
+      type: Object,
+      default: undefined
+    }
+  },
+
   components: {
     EditProfileForm
   }
