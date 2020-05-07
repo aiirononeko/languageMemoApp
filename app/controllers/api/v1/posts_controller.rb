13d3d8f1ecec7de74f3fd97115d5110c34d7226e
榑林晃平 <kureykou321@gmail.com>
2020-05-08 03:14:03 +0900

    fix-post-function

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   before_action :authenticate_api_v1_user!, except:[:show]
-  before_action :set_post, except: [:new, :create]
+  before_action :set_post, except: [:new,:create]
   before_action :correct_user?, only: [:update, :destroy]
 
   def show
@@ -13,12 +13,16 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def create
-    @post = current_api_v1_user.posts.build(post_params)
-    if @post.save
-      render json: @post, serializer: PostSerializer
-    else
-      render json: { status: "error", errors: @post.errors }
-    end
+   if params[:folder_id] === nil
+      @post2 = current_api_v1_user.posts.build(no_folder_params)
+      @post2.save
+      render json: @post2, serializer: PostSerializer
+   else
+      @folder = Folder.joins(:user).where(user_id: @current_api_v1_user.id).find(params[:folder_id])
+      @post1 =  @folder.posts.build(post_params)
+      @post1.save
+      render json: @post1, serializer: PostSerializer
+   end
   end
 
   def edit
@@ -34,6 +38,7 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def destroy
+    #binding.pry
     if @post.destroy
       render json: @post, serializer: PostSerializer
     else
@@ -44,7 +49,11 @@ class Api::V1::PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:name, :content, :public)
+    params.require(:post).permit(:name, :content, :public,:user_id, :folder_id)
+  end
+
+  def no_folder_params
+    params.require(:post).permit(:name, :content, :public, :user_id)
   end
 
   def set_post
@@ -52,6 +61,7 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def correct_user?
+    #binding.pry
     return if current_api_v1_user == @post.user
     render json: { success: false,
                    errors: ["You don't have the right to access this resource"] }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :folder
+  belongs_to :folder, optional: true
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :content, presence: true
