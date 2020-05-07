9ce5ad51d67420539a75957800588ce1ab788235
Kanezoh <asfaltmanta114@gmail.com>
2020-05-07 15:03:52 +0900

    Githubのリポジトリ作成権限を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile'
+  provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'], scope: 'user, repo', provider_ignores_state: true
   provider :twitter,       ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET']
 end
