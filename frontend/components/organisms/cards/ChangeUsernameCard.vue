617290468d0683e2489056f9ca3551de4749c041
show <show75@ezweb.ne.jp>
2020-05-10 19:34:12 +0900

    修正

--- frontend/components/organisms/cards/ChangeUsernameCard.vue
+++ frontend/components/organisms/cards/ChangeUsernameCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="d-flex flex-column justify-center align-center mx-auto" width="500" height="300">
+  <v-card class="d-flex flex-column justify-center align-center mx-auto" max-width="500" height="300">
     <p class="mb-5">ユーザーIDを入力してください</p>
     <p class="mb-5">※半角英数字とアンダーバー(_)のみ使用可能です。</p>
 
