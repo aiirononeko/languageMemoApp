cf8e0da769e9c699aaeebdedbffea5d965a689ec
榑林晃平 <kureykou321@gmail.com>
2020-05-11 22:56:03 +0900

    strong_paramsの命名変更

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,4 +2,7 @@ class Folder < ApplicationRecord
   belongs_to :user
 
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :public, inclusion: { in: [true, false] }
 end
