ebd814cefb83ea17b614b065eec1a8c5fab9b469
H37kouya <h37kouya@gmail.com>
2020-05-12 11:41:55 +0900

    ログインのURLをsigninに統一

--- frontend/middleware/authenticated.js
+++ frontend/middleware/authenticated.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
     // ユーザーが認証されていないとき
   if (!store.getters["authentication/isAuthenticated"]) {
-    return redirect('/user/login')
+    return redirect('/signin')
   }
 }
