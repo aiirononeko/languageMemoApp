901a48f4f9e77f38015e2cd188a9d1fa689272ed
榑林晃平 <kureykou321@gmail.com>
2020-05-13 02:14:47 +0900

    requestのposts_spec.rbにfolderの有無の条件を設定した。

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -2,47 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Posts", type: :request do
   describe "GET /api/v1/posts/:id" do
-    subject(:call_api){ get "/api/v1/posts/#{post.id}" }
+    subject(:call_api){ get "/api/v1/users/#{user.id}/posts/#{post.id}" }
 
     let(:user) { create(:confirmed_user) }
-    let(:post) { create :post, user_id: user.id }
 
-    it "レスポンスボディーに期待された値が返ること" do
-      call_api
-      res = JSON.parse(response.body)
-      expect(res["data"]["attributes"]["name"]).to eq "test"
-      expect(res["data"]["attributes"]["content"]).to eq "example"
-      expect(res["data"]["attributes"]["public"]).to eq false
-      expect(res["data"]["attributes"]["user-id"]).to eq user.id
-    end
-  end
-
-  describe "POST /api/v1/posts" do
-    subject(:call_api){ post '/api/v1/posts', headers: headers, params: params }
-
-    let(:user) { create(:confirmed_user) }
-    let(:headers) { user.create_new_auth_token }
-    let(:params) {{ }}
-
-    context '投稿の作成に成功した場合' do
-      let(:params) do
-        {
-          post: {
-            name: "test",
-            content: "example",
-            public: false,
-          }
-        }
-      end
-
-      it 'レスポンスステータスが200で返ること' do
-        call_api
-        expect(response.status).to eq 200
-      end
-
-      it '投稿が作成されていること' do
-        expect { call_api }.to change { Post.count }.from(0).to(1)
-      end
+    context "folderが存在している場合" do
+      let(:folder) { create :folder, user_id: user.id }
+      let(:post) { create :post, user_id: user.id, folder_id: folder.id }
 
       it "レスポンスボディーに期待された値が返ること" do
         call_api
@@ -51,6 +17,98 @@ RSpec.describe "Api::V1::Posts", type: :request do
         expect(res["data"]["attributes"]["content"]).to eq "example"
         expect(res["data"]["attributes"]["public"]).to eq false
         expect(res["data"]["attributes"]["user-id"]).to eq user.id
+        expect(res["data"]["attributes"]["folder-id"]).to eq folder.id
+      end
+    end
+
+    context "folderが存在していない場合" do
+      let(:post) { create :post, user_id: user.id, folder_id: nil }
+
+      it "レスポンスボディーに期待された値が返ること" do
+        call_api
+        res = JSON.parse(response.body)
+        expect(res["data"]["attributes"]["name"]).to eq "test"
+        expect(res["data"]["attributes"]["content"]).to eq "example"
+        expect(res["data"]["attributes"]["public"]).to eq false
+        expect(res["data"]["attributes"]["user-id"]).to eq user.id
+        expect(res["data"]["attributes"]["folder-id"]).to eq nil
+      end
+    end
+  end
+
+  describe "POST /api/v1/posts" do
+    subject(:call_api){ post "/api/v1/users/#{user.id}/posts", headers: headers, params: params }
+
+    let(:user) { create(:confirmed_user) }
+    let(:headers) { user.create_new_auth_token }
+    let(:params) {{ }}
+
+    context '投稿の作成に成功した場合' do
+      context "folderが存在している場合" do
+        let(:folder) { create :folder, user_id: user.id }
+        let(:params) do
+          {
+            post: {
+              name: "test",
+              content: "example",
+              public: false,
+              user_id: user.id,
+              folder_id: folder.id
+            }
+          }
+        end
+
+        it 'レスポンスステータスが200で返ること' do
+          call_api
+          expect(response.status).to eq 200
+        end
+
+        it '投稿が作成されていること' do
+          expect { call_api }.to change { Post.count }.from(0).to(1)
+        end
+
+        it "レスポンスボディーに期待された値が返ること" do
+          call_api
+          res = JSON.parse(response.body)
+          expect(res["data"]["attributes"]["name"]).to eq "test"
+          expect(res["data"]["attributes"]["content"]).to eq "example"
+          expect(res["data"]["attributes"]["public"]).to eq false
+          expect(res["data"]["attributes"]["user-id"]).to eq user.id
+          expect(res["data"]["attributes"]["folder-id"]).to eq folder.id
+        end
+      end
+
+      context "folderが存在していない場合" do
+        let(:params) do
+          {
+            post: {
+              name: "test",
+              content: "example",
+              public: false,
+              user_id: user.id,
+              folder_id: nil
+            }
+          }
+        end
+
+        it 'レスポンスステータスが200で返ること' do
+          call_api
+          expect(response.status).to eq 200
+        end
+
+        it '投稿が作成されていること' do
+          expect { call_api }.to change { Post.count }.from(0).to(1)
+        end
+
+        it "レスポンスボディーに期待された値が返ること" do
+          call_api
+          res = JSON.parse(response.body)
+          expect(res["data"]["attributes"]["name"]).to eq "test"
+          expect(res["data"]["attributes"]["content"]).to eq "example"
+          expect(res["data"]["attributes"]["public"]).to eq false
+          expect(res["data"]["attributes"]["user-id"]).to eq user.id
+          expect(res["data"]["attributes"]["folder-id"]).to eq nil
+        end
       end
     end
 
@@ -76,7 +134,7 @@ RSpec.describe "Api::V1::Posts", type: :request do
   end
 
   describe "PUT /api/v1/posts/:id" do
-    subject(:call_api) { put "/api/v1/posts/#{post.id}", headers: headers, params: params }
+    subject(:call_api) { put "/api/v1/users/#{user.id}/posts/#{post.id}", headers: headers, params: params }
 
     let(:user) { create :confirmed_user }
     let(:user2) { create :confirmed_user, username: "test_name" }
@@ -86,32 +144,68 @@ RSpec.describe "Api::V1::Posts", type: :request do
 
     context 'ログインユーザーと投稿ユーザーが一致している場合' do
       context '投稿の更新に成功した場合' do
-        let(:params) do
-          {
-            post: {
-              name: "example",
-              content: "example",
-              public: false,
+        context "folderが存在している場合" do
+          let(:folder) { create :folder, user_id: user.id }
+          let(:params) do
+            {
+              post: {
+                name: "example",
+                content: "example",
+                public: false,
+                user_id: user.id,
+                folder_id: folder.id
+              }
             }
-          }
+          end
+
+          it 'レスポンスステータスが200で返ること' do
+            call_api
+            expect(response.status).to eq 200
+          end
+
+          it "レスポンスボディーに期待された値が返ること" do
+            call_api
+            res = JSON.parse(response.body)
+            expect(res["data"]["attributes"]["name"]).to eq "example"
+            expect(res["data"]["attributes"]["content"]).to eq "example"
+            expect(res["data"]["attributes"]["public"]).to eq false
+            expect(res["data"]["attributes"]["user-id"]).to eq user.id
+            expect(res["data"]["attributes"]["folder-id"]).to eq folder.id
+          end
         end
 
-        it 'レスポンスステータスが200で返ること' do
-          call_api
-          expect(response.status).to eq 200
-        end
+        context "folderが存在していない場合" do
+          let(:params) do
+            {
+              post: {
+                name: "example",
+                content: "example",
+                public: false,
+                user_id: user.id,
+                folder_id: nil
+              }
+            }
+          end
 
-        it "レスポンスボディーに期待された値が返ること" do
-          call_api
-          res = JSON.parse(response.body)
-          expect(res["data"]["attributes"]["name"]).to eq "example"
-          expect(res["data"]["attributes"]["content"]).to eq "example"
-          expect(res["data"]["attributes"]["public"]).to eq false
-          expect(res["data"]["attributes"]["user-id"]).to eq user.id
+          it 'レスポンスステータスが200で返ること' do
+            call_api
+            expect(response.status).to eq 200
+          end
+
+          it "レスポンスボディーに期待された値が返ること" do
+            call_api
+            res = JSON.parse(response.body)
+            expect(res["data"]["attributes"]["name"]).to eq "example"
+            expect(res["data"]["attributes"]["content"]).to eq "example"
+            expect(res["data"]["attributes"]["public"]).to eq false
+            expect(res["data"]["attributes"]["user-id"]).to eq user.id
+            expect(res["data"]["attributes"]["folder-id"]).to eq nil
+          end
         end
       end
 
       context '投稿の更新に失敗した場合' do
+        let(:folder) { create :folder, user_id: user.id }
         let(:params) do
           {
             post: {
@@ -133,27 +227,29 @@ RSpec.describe "Api::V1::Posts", type: :request do
     end
 
     context 'ログインユーザーと投稿ユーザーが一致していない場合' do
-        before { post.update(user_id: user2.id) }
+      before { post.update(user_id: user2.id) }
 
-        it "アクセス権限がないと言われる" do
-          call_api
-          res = JSON.parse(response.body)
-          expect(res["success"]).to eq false
-          expect(res["errors"]).to eq ["You don't have the right to access this resource"]
-        end
+      it "アクセス権限がないと言われる" do
+        call_api
+        res = JSON.parse(response.body)
+        expect(res["success"]).to eq false
+        expect(res["errors"]).to eq ["You don't have the right to access this resource"]
+      end
     end
   end
 
   describe "DELETE /api/v1/posts/:id" do
-    subject(:call_api) { delete "/api/v1/posts/#{post.id}", headers: headers }
+    subject(:call_api) { delete "/api/v1/users/#{user.id}/posts/#{post.id}", headers: headers }
 
     let(:user) { create :confirmed_user }
     let(:user2) { create :confirmed_user, username: "test_name" }
     let(:headers) { user.create_new_auth_token }
-    let(:post) { create :post, user_id: user.id }
+    let(:folder) { create :folder, user_id: user.id }
+    let(:post) { create :post, user_id: user.id, folder_id: folder.id }
 
     context 'ログインユーザーと投稿ユーザーが一致している場合' do
       context '投稿の削除に成功した場合' do
+
         it 'レスポンスステータスが200で返ること' do
           call_api
           expect(response.status).to eq 200
@@ -170,6 +266,7 @@ RSpec.describe "Api::V1::Posts", type: :request do
           expect(res["data"]["attributes"]["content"]).to eq "example"
           expect(res["data"]["attributes"]["public"]).to eq false
           expect(res["data"]["attributes"]["user-id"]).to eq user.id
+          expect(res["data"]["attributes"]["folder-id"]).to eq folder.id
         end
       end
     end
