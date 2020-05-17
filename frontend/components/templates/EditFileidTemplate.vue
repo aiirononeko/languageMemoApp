3ae4700a10fcdab607273ac99b2f1fbc6422429c
H37kouya <h37kouya@gmail.com>
2020-05-17 17:16:25 +0900

    feat 投稿するボタンの追加

--- frontend/components/templates/EditFileidTemplate.vue
+++ frontend/components/templates/EditFileidTemplate.vue
@@ -1,15 +1,21 @@
 <template>
-  <one-column-container fluid>
+  <one-column-container class="pos-relative" fluid>
     <edit-markdown v-model="md" />
+
+    <blue-btn class="post-btn" large @click="post">
+      投稿する
+    </blue-btn>
   </one-column-container>
 </template>
 
 <script>
+const BlueBtn = () => import('~/components/atoms/btns/BlueBtn')
 const EditMarkdown = () => import('~/components/organisms/markdown/EditMarkdown')
 const OneColumnContainer = () => import('~/components/molecules/containers/OneColumnContainer')
 
 export default {
   components: {
+    BlueBtn,
     EditMarkdown,
     OneColumnContainer
   },
@@ -35,6 +41,21 @@ export default {
         return this.$emit('input', newVal)
       }
     }
-  }
+  },
+
+  methods: {
+    post() {
+      return this.$emit('post')
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.post-btn {
+  position: fixed;
+
+  bottom: 5vh;
+  right: 5vw;
+}
+</style>
