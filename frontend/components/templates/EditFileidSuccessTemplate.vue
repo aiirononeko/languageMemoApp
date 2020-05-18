d32ee637731ca22018f13c1ebee74cadfa5d5a3c
H37kouya <h37kouya@gmail.com>
2020-05-18 11:21:22 +0900

    feat 投稿するボタンで完了ページにリダイレクトするようにした

---
+++ frontend/components/templates/EditFileidSuccessTemplate.vue
@@ -0,0 +1,39 @@
+<template>
+  <one-column-container>
+    <h1 class="text-center mb-4">投稿完了しました</h1>
+
+    <v-row class="mb-4" justify="center">
+      <twitter-btn>
+        Twitterに投稿する
+      </twitter-btn>
+    </v-row>
+
+    <v-row class="mb-4" justify="center">
+      <orange-btn class="mr-2">
+        編集する
+      </orange-btn>
+
+      <orange-btn>
+        投稿を見る
+      </orange-btn>
+    </v-row>
+  </one-column-container>
+</template>
+
+<script>
+const TwitterBtn = () => import('~/components/molecules/btns/TwitterBtn')
+const OneColumnContainer = () => import('~/components/molecules/containers/OneColumnContainer')
+const OrangeBtn = () => import('~/components/atoms/btns/OrangeBtn')
+
+export default {
+  components: {
+    TwitterBtn,
+    OneColumnContainer,
+    OrangeBtn
+  }
+}
+</script>
+
+<style>
+
+</style>
