13718e22e699ca4b70c248175b8835887d81b70a
Kanezoh <asfaltmanta114@gmail.com>
2020-05-19 10:34:20 +0900

    feat folderのjsonに直下のpostを含めるようにする

--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -1,3 +1,3 @@
 class FolderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id, :public, :created_at, :updated_at
+  attributes :id, :name, :user_id, :public, :created_at, :updated_at, :posts
 end
