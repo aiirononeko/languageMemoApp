6f71eb88c7ba7c29d8fea14c3e3631492c7e5126
Kanezoh <asfaltmanta114@gmail.com>
2020-05-21 17:03:55 +0900

    fix verifying csrfを解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
+  #skip_before_action :verify_authenticity_token
 end
