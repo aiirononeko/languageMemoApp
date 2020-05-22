26202b188f6cab95d55e129b5a8f4f9f4cac429a
show <show75@ezweb.ne.jp>
2020-05-22 15:05:04 +0900

    feat: モックを作成し、UI整理

--- frontend/components/organisms/list/FileFolderListWithAction.vue
+++ frontend/components/organisms/list/FileFolderListWithAction.vue
@@ -3,13 +3,14 @@
     <!-- ファイルやフォルダーのリスト -->
     <v-list>
       <v-list-item-group>
-        <v-list-item>
+        <v-list-item  v-for="(item, i) in list" :key="i">
           <v-list-item-icon>
-            <v-icon>mdi-folder</v-icon>
+            <v-icon v-if="item.isFolder">mdi-folder</v-icon>
+            <v-icon v-else>mdi-file</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>
-              Docker入門
+              {{ item.title }}
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
@@ -29,7 +30,36 @@ export default {
     FileListItem,
     FolderListItem,
     LinkToBackItem
-  }
+  },
+
+  props: {
+    // リストを受けとる
+    // list: {
+    //   type: Array,
+    //   default: undefined
+    // }
+  },
+
+  data: () => ({
+    list: [
+      {
+        title: "Docker",
+        isFolder: true
+      },
+      {
+        title: "CSS",
+        isFolder: true
+      },
+      {
+        title: "HTML",
+        isFolder: true
+      },
+      {
+        title: "MaterialDesgin.md",
+        isFolder: false
+      },
+    ]
+  }),
 }
 </script>
 
