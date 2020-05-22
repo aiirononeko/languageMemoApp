67be632ae74b2e3fc64aa40f00581806a7fe12c0
Kanezoh <asfaltmanta114@gmail.com>
2020-05-22 13:51:00 +0900

    feat ユーザーのshowアクションのパラメータをid→usernameに

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:username])
     render json: @user, serializer: UserSerializer
   end
 end
