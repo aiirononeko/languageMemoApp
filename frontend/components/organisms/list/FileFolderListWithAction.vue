a0ec3706f5df6245a4d1b60ef5666961d69c9cd5
show <show75@ezweb.ne.jp>
2020-05-22 11:23:34 +0900

    feat: ファイルフォルダーリストの基礎作成

--- frontend/components/organisms/list/FileFolderListWithAction.vue
+++ frontend/components/organisms/list/FileFolderListWithAction.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <!-- ファイルやフォルダーのリスト -->
+    <v-list>
+      <v-list-item-group>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-folder</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              Docker入門
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
     <!-- 編集や削除のアクションを付けるList -->
   </div>
 </template>
