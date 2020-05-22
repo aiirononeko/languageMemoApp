e2fc1aef05ce36d246ac8a05cad933766fe0feee
H37kouya <h37kouya@gmail.com>
2020-05-22 12:11:40 +0900

    chore nuxt.configの内容を更新

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,19 +9,14 @@ module.exports = {
    ** Headers of the page
    */
   head: {
+    htmlAttrs: { lang: "ja" },
     title: "frontend",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "Nuxt.js project" },
     ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        href: "https://use.fontawesome.com/releases/v5.6.1/css/all.css",
-        rel: "stylesheet",
-      },
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   /*
@@ -34,14 +29,24 @@ module.exports = {
    */
   modules: [
     "@nuxtjs/axios",
-    "@nuxtjs/vuetify",
     "@nuxtjs/device",
-    "@nuxtjs/auth",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/vuetify",
     ["nuxt-sass-resources-loader", ["~/assets/sass/variable.scss"]],
   ],
 
-  buildModules: ["@nuxtjs/dotenv"],
+  /*
+   ** build modules
+   */
+  buildModules: [
+    "@nuxtjs/dotenv",
+    // "@nuxtjs/google-analytics",
+    // "@nuxtjs/pwa" // pwaは開発時にバグりやすいので、しばらくはコメント,
+  ],
 
+  /*
+   ** plugins
+   */
   plugins: [
     "~/plugins/axios",
     { src: "~/plugins/vue-mavon-editor", ssr: false },
@@ -49,15 +54,36 @@ module.exports = {
 
   css: [{ src: "~/assets/sass/common.scss", lang: "scss" }],
 
+  /*
+   ** Axios configuration
+   */
   axios: {
     // axios options
     baseURL: process.env.BASE_URL || "http://localhost:3000",
   },
 
   /*
+   ** Google Analytics configuration
+   */
+  // googleAnalytics: {
+  //   // google analytics options
+  //   id: 'UA-12301-2'
+  // },
+
+  /*
+   ** Sitemap configuration
+   */
+  sitemap: {
+    // sitemap options
+    hostname: process.env.BASE_URL || "http://localhost:3000",
+    gzip: true,
+  },
+
+  /*
    ** Build configuration
    */
   build: {
+    extractCSS: true,
     /*
      ** Run ESLint on save
      */
@@ -68,8 +94,8 @@ module.exports = {
           test: /\.(js|vue)$/,
           loader: "eslint-loader",
           exclude: /(node_modules)/,
-        })
+        });
       }
     },
   },
-}
+};
