d2a11617920dfdd8893a956051a4e60840e5f666
Kanezoh <asfaltmanta114@gmail.com>
2020-05-23 00:06:55 +0900

    feat serializerの設定

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
+  include Concerns::ImageUrl
   attributes :id, :name, :username, :image, :profile, :address, :twitter_link, :github_link, :created_at, :updated_at
+
+  def image
+    image = url_for(object.avatar) if object.avatar.attached?
+    image = object.image unless object.avatar.attached?
+    image
+  end
 end
