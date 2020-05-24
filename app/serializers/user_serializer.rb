03d52815b6cdb500ab7d1575bc3c26972041ca8e
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2020-05-24 11:53:35 +0900

    ユーザーアイコンの実装 (#153)
    * ActiveStorageの初期設定
    
    * feat:画像アップロード機能の設定
    
    * feat 受け取った画像ファイルの処理
    
    * feat storageをGCPの設定
    
    * 開発環境の画像アップロード先をGCPに
    
    * feat serializerの設定
    
    * fix 一時ファイル保存場所の変更
    
    * feat 改行の修正
    
    * feat テスト用の設定
    
    * test avatarのアップロードテストを追加
    
    * feat デフォルトURLの設定
    
    * fix 不要な設定の削除
    
    * refact 不要なクエリを送らないようにする
    
    * feat デフォルトURLの設定
    
    * refact imageメソッドのロジックを改良
    
    * test avatarテストの追加
    
    * chore gitignoreにjsonを無視する設定を追加

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
+  include Concerns::ImageUrl
   attributes :id, :name, :username, :image, :profile, :address, :twitter_link, :github_link, :created_at, :updated_at
+
+  def image
+    object.avatar.attached? ?  url_for(object.avatar) : object.image
+  end
 end
