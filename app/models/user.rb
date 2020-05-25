3ff42d966352f68043b11f76ee2740b7e41bb439
Kanezoh <asfaltmanta114@gmail.com>
2020-05-25 19:28:28 +0900

    feat avatarのモデルバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,15 @@ class User < ActiveRecord::Base
   validates :address, length: { maximum: 30 }
   validates :username, length: { maximum: 30 }, uniqueness: true, allow_nil: true, presence: true, format: { with: VALID_USERNAME_REGEX }
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
+
+  validate :avatar_content_type, if: :was_attached?
+
+  def avatar_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+    errors.add(:avatar, "の拡張子が間違っています") unless avatar.content_type.in?(extension)
+  end
+
+  def was_attached?
+    self.avatar.attached?
+  end
 end
