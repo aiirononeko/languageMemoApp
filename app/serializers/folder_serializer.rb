36691606ac5916590f4b5b9bf0908ee787750c12
Kanezoh <asfaltmanta114@gmail.com>
2020-05-28 21:32:31 +0900

    feat コントローラ、シリアライザーの設定

--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -1,3 +1,3 @@
 class FolderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id, :public, :created_at, :updated_at, :posts
+  attributes :id, :name, :user_id, :parent_id, :public, :created_at, :updated_at, :posts
 end
