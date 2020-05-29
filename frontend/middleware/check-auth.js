cc143ea8754c8af2d5dfdee1589df7842c8a9eab
H37kouya <h37kouya@gmail.com>
2020-05-29 20:15:03 +0900

    feat User型の追加と認証周りの追加と修正。

--- frontend/middleware/check-auth.js
+++ frontend/middleware/check-auth.js
@@ -1,9 +1,9 @@
-// export default async ({ store, req }) => {
-//   // ログイン状態でuserInfoがないとき、取得する
-//   if (
-//     store.getters["authentication/isAuthenticated"] &&
-//     !store.getters["authentication/userInfo"]
-//   ) {
-//     await store.dispatch("authentication/fetchUser")
-//   }
-// }
+export default async ({ store }) => {
+  // ログイン状態でuserInfoがないとき、取得する
+  if (
+    store.getters["authentication/isAuthenticated"] &&
+    !store.getters["authentication/userInfo"]
+  ) {
+    await store.dispatch("authentication/fetchUser")
+  }
+}
