3ff0ff24a8e53604d900aa9375e1e197bf94e8e6
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2020-05-29 13:09:39 +0900

    folderにparent_idを追加 (#158)
    * feat folderテーブルにparent_idを追加
    
    * feat モデルにparent_idのバリデーション追加
    
    * feat コントローラ、シリアライザーの設定
    
    * feat schema.rbの変更
    
    * fix rspecが途中終了する不具合を解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_011753) do
+ActiveRecord::Schema.define(version: 2020_05_28_122241) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_011753) do
     t.boolean "public", default: true, null: false, comment: "公開or非公開"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "parent_id"
     t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
