027c293e114622853405fa4330bfc71354686b7b
H37kouya <h37kouya@gmail.com>
2020-05-29 20:12:46 +0900

    fix axiosでheaderにデータがあったりなかったりするので、修正した

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,7 +1,9 @@
-export default ({ app: { $axios }, store }) => {
+export default ({ $axios , store }) => {
   $axios.onRequest(config => {
-    $axios.setHeader("access-token", store.getters["authentication/accessToken"])
-    $axios.setHeader("uid", store.getters["authentication/uid"])
-    $axios.setHeader("client", store.getters["authentication/client"])
+    if (store.getters["authentication/isAuthenticated"]) {
+      config.headers.common["access-token"] = store.getters["authentication/accessToken"]
+      config.headers.common["uid"] = store.getters["authentication/uid"]
+      config.headers.common["client"] = store.getters["authentication/client"]
+    }
   })
 }
