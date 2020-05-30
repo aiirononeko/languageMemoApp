ab1b273ee35c6ee3b43d668be3b883ace9c92dbf
Kanezoh <asfaltmanta114@gmail.com>
2020-05-30 21:40:36 +0900

    fix parent_folderの削除

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,9 +2,7 @@ class Folder < ApplicationRecord
   belongs_to :user
 
   has_many :posts, dependent: :destroy
-  has_many :parent_relationships, dependent: :destroy, foreign_key: "parent_id", class_name: "FolderRelationship"
   has_many :child_relationships, dependent: :destroy, foreign_key: "child_id", class_name: "FolderRelationship"
-  has_many :parent_folder, through: :parent_relationships, source: :parent
   has_many :child_folders, through: :child_relationships, source: :child
 
   validates :name, presence: true, length: { maximum: 50 }
