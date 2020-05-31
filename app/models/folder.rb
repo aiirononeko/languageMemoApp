eca0aa1c1ba7c0e69dd58997d3f90e375c2b3734
Kanezoh <asfaltmanta114@gmail.com>
2020-05-31 17:31:11 +0900

    feat jsonでparent_folderとchild_foldersを返すよう設定

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,7 +2,7 @@ class Folder < ApplicationRecord
   belongs_to :user
 
   has_many :posts, dependent: :destroy
-  has_many :child_relationships, dependent: :destroy, foreign_key: "child_id", class_name: "FolderRelationship"
+  has_many :child_relationships, dependent: :destroy, foreign_key: "parent_id", class_name: "FolderRelationship"
   has_many :child_folders, through: :child_relationships, source: :child
 
   validates :name, presence: true, length: { maximum: 50 }
--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -1,3 +1,7 @@
 class FolderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id, :parent_id, :public, :created_at, :updated_at, :posts
+  attributes :id, :name, :user_id, :public, :created_at, :updated_at, :posts, :parent_folder, :child_folders
+
+  def parent_folder
+    Folder.find_by(id: object.parent_id)
+  end
 end
