0a21ed3b2b3c5c42e5f3bf20618a59142e6fc072
Kanezoh <asfaltmanta114@gmail.com>
2020-05-31 19:13:00 +0900

    feat dependent destroyの設定

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,7 +3,7 @@ class Folder < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :child_relationships, dependent: :destroy, foreign_key: "parent_id", class_name: "FolderRelationship"
-  has_many :child_folders, through: :child_relationships, source: :child
+  has_many :child_folders, through: :child_relationships, source: :child, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :public, inclusion: { in: [true, false] }
