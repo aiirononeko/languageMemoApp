3b9f8e82c8bd12fc4124aa53c66fc911cc2e4dae
H37kouya <h37kouya@gmail.com>
2020-06-02 12:51:22 +0900

    feat /password/reset, /password/reset/confirmの追加

---
+++ frontend/components/organisms/form/PasswordResetConfirmForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-form @submit="onSubmit">
+    <password-confirmation-text-field
+      v-model="password"
+      :cvalue="password_confirmation"
+      @cinput="onCInput"
+    />
+
+    <div class="d-flex justify-end">
+      <blue-btn>
+        送信
+      </blue-btn>
+    </div>
+  </v-form>
+</template>
+
+<script>
+const BlueBtn = () => import('~/components/atoms/btns/BlueBtn')
+const PasswordConfirmationTextField = () => import('~/components/organisms/textFields/PasswordConfirmationTextField')
+
+export default {
+  components: {
+    BlueBtn,
+    PasswordConfirmationTextField
+  },
+
+  data() {
+    return {
+      password: null,
+      password_confirmation: null
+    }
+  },
+
+  methods: {
+    onCInput(newVal) {
+      this.password_confirmation = newVal
+    },
+
+    onSubmit() {
+      return this.$emit('submit', this.email)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
