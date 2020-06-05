f09432417ba7736743e9f3ba2aaab01b6007cd42
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2020-06-06 00:50:24 +0900

    Api仕様書の整理+α (#162)
    * feat userが直下のpost,folderを返すようにする
    
    * style 仕様書の整理
    
    * style password関連APIのリクエストに必要なヘッダ情報を記載
    
    * style 記載ミスを修正

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
   include Concerns::ImageUrl
-  attributes :id, :name, :username, :image, :profile, :address, :twitter_link, :github_link, :created_at, :updated_at
+  attributes :id, :name, :username, :image, :profile, :posts, :folders, :address, :twitter_link, :github_link, :created_at, :updated_at
 
   def image
     object.avatar.attached? ?  url_for(object.avatar) : object.image
+  end
+
+  def posts
+    object.posts.where(folder_id: nil)
+  end
+
+  def folders
+    object.folders.where(parent_id: nil)
   end
 end
