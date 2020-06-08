d1429dca8e2af4e9372759915efc26af29fa7dc3
show <show75@ezweb.ne.jp>
2020-06-08 19:41:34 +0900

    refactor: filelistとfolderlistを切り出し

---
+++ frontend/components/organisms/list/FolderListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list-item>
+    <v-list-item-icon>
+      <v-icon>mdi-folder</v-icon>
+    </v-list-item-icon>
+
+    <v-list-item-content>
+      <v-list-item-title>
+          {{ name }}
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
