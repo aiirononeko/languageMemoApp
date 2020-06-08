e328549a446ebf724bdf354fb2a2407b93310a94
H37kouya <h37kouya@gmail.com>
2020-06-08 12:59:46 +0900

    feat successでもfileIDが正しいかを確認するようにした

--- frontend/pages/edit/success.vue
+++ frontend/pages/edit/success.vue
@@ -5,14 +5,26 @@
 </template>
 
 <script>
+import {isCurrentFileID} from "~/utils/fileID"
 const EditFileidSuccessTemplate = () => import('~/components/templates/EditFileidSuccessTemplate')
+
+// 不正な fileID だったら、APIの送信に辿り着く前に弾く
+const checkCurrentFileID = ({ query, redirect }) => {
+  // 不正なfileID (数値以外) 出ないことを確認
+  if (!query.fileid || !isCurrentFileID(query.fileid)) {
+    return redirect('/edit/new') // 新規作成ページにリダイレクト
+  }
+}
 
 export default {
   components: {
     EditFileidSuccessTemplate
   },
 
-  middleware: "authenticated",
+  middleware: [
+    "authenticated",
+    checkCurrentFileID
+  ],
 
   computed: {
     fileID() {
