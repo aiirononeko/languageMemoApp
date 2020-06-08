619b65078371b2037cc14a8bf232182392eacdcd
Kanezoh <asfaltmanta114@gmail.com>
2020-06-08 10:48:15 +0900

    feat リソースが見つからない場合、404エラーを返す

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
-    render json: @user, serializer: UserSerializer
+    if @user
+      render json: @user, serializer: UserSerializer
+    else
+      head :not_found
+    end
   end
 end
