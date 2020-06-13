23d9f901fd793845fd5cd518567cd12d8c42ed6d
Kanezoh <47489839+Kanezoh@users.noreply.github.com>
2020-06-13 12:20:42 +0900

    先祖フォルダーの情報を配列で返す (#175)
    * feat 先祖フォルダーの値を配列で返す
    
    * test 先祖フォルダーのテストを追加
    
    * style 仕様書を修正
    
    * feat postでも先祖フォルダーを返す
    
    * test postで先祖フォルダーのテスト
    
    * style 仕様書を修正

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,16 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :name, :content, :public, :user_id, :folder_id, :created_at, :updated_at
+  attributes :id, :name, :content, :public, :user_id, :ancestor_folders, :created_at, :updated_at
+
+  def ancestor_folders
+    ancestor_folders = []
+    return nil if object.folder_id.nil?
+    folder = Folder.find_by(id: object.folder_id)
+    ancestor_folders << folder
+    loop do
+      folder = Folder.find_by(id: folder.parent_id)
+      break if folder.nil?
+      ancestor_folders << folder
+    end
+    ancestor_folders
+  end
 end
