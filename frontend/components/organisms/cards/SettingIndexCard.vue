8af3d5a902ec203d11fc0ea7b531aeba7c739d88
H37kouya <h37kouya@gmail.com>
2020-06-19 15:15:34 +0900

    feat /settingsのページの追加と/settings/**のデザインを修正

---
+++ frontend/components/organisms/cards/SettingIndexCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-list>
+      <v-list-item to="/settings/profile">
+        プロフィール編集
+      </v-list-item>
+
+      <v-list-item to="/settings/user-name">
+        ユーザーIDの変更
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
