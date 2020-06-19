fe5b638f52aaa8c3cc96c5329e88fdb3802c7518
H37kouya <h37kouya@gmail.com>
2020-06-19 17:53:26 +0900

    feat /settings/deactivateの追加

--- frontend/components/atoms/btns/RedBtn.vue
+++ frontend/components/atoms/btns/RedBtn.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-btn depressed color="red lighten-2" class="btn" :disabled="disabled" :large="large" :to="to">
+  <v-btn
+    :disabled="disabled"
+    :large="large"
+    :to="to"
+    @click="onClick"
+    depressed
+    color="red lighten-2"
+    class="btn"
+  >
     <slot/>
   </v-btn>
 </template>
@@ -20,6 +28,12 @@ export default {
     to: {
       type: [String, Object],
       default: undefined
+    }
+  },
+
+  methods: {
+    onClick() {
+      return this.$emit('click')
     }
   },
 }
---
+++ frontend/components/templates/SettingsDeactivateTemplate.vue
@@ -0,0 +1,51 @@
+<template>
+  <one-column-container>
+    <h1 class="main-heading mb-8 text-center">アカウント削除</h1>
+
+    <v-row justify="center">
+      <v-col cols="12" sm="10" md="8">
+        <setting-deactivate-card v-if="!success" @deactivate="onDeactivate" />
+
+        <v-alert
+          border="top"
+          colored-border
+          type="success"
+          elevation="2"
+          v-else
+        >
+          {{ message }}
+        </v-alert>
+      </v-col>
+    </v-row>
+  </one-column-container>
+</template>
+
+<script>
+const OneColumnContainer = () => import('~/components/molecules/containers/OneColumnContainer')
+const SettingDeactivateCard = () => import('~/components/organisms/cards/SettingDeactivateCard')
+
+export default {
+  components: {
+    OneColumnContainer,
+    SettingDeactivateCard
+  },
+
+  props: {
+    success: {
+      type: Boolean,
+      default: false
+    },
+
+    message: {
+      type: String,
+      default: undefined
+    }
+  },
+
+  methods: {
+    onDeactivate() {
+      return this.$emit('deactivate')
+    }
+  },
+}
+</script>
