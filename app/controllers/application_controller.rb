111f2a4c6704c96dc951239e01b851d125353caf
Kanezoh <asfaltmanta114@gmail.com>
2020-06-21 11:10:51 +0900

    fix corsの設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token, :only => [:update]
 end
