14321f934c4ca4862a8059b7a81d31c57f45146f
Kanezoh <asfaltmanta114@gmail.com>
2020-06-23 17:34:07 +0900

    fix invalid signatureエラーの解決

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -7,19 +7,20 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   end
 
   def update
-    if params[:avatar]
-      image_match = params[:avatar].match(/^data:(.*?);(?:.*?),(.*)$/)
-      mime_type, encoded_image = image_match.captures
-      extension = mime_type.split('/').second
-      decoded_image = Base64.decode64(encoded_image)
-      filename = "avatar#{current_api_v1_user.id}.#{extension}"
-      image_path = Rails.root.join('storage', filename)
-      File.open(image_path, 'wb') do |f|
-        f.write(decoded_image)
+    if @resource
+      if params[:avatar]
+        upload_image(@resource, params[:avatar])
+        params.delete :avatar
       end
-      current_api_v1_user.avatar.attach({ io: File.open(image_path), filename: filename, content_type: mime_type })
+      if @resource.send(resource_update_method, account_update_params)
+        yield @resource if block_given?
+        render_update_success
+      else
+        render_update_error
+      end
+    else
+      render_update_error_user_not_found
     end
-    super
   end
 
   private
@@ -43,6 +44,21 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   def set_default_username
     default_username = "user" + SecureRandom.alphanumeric(15)
     @resource.update
+  end
+
+  def upload_image(user, avatar)
+    if avatar
+      image_match = avatar.match(/^data:(.*?);(?:.*?),(.*)$/)
+      mime_type, encoded_image = image_match.captures
+      extension = mime_type.split('/').second
+      decoded_image = Base64.decode64(encoded_image)
+      filename = "avatar#{user.id}.#{extension}"
+      image_path = Rails.root.join('storage', filename)
+      File.open(image_path, 'wb') do |f|
+        f.write(decoded_image)
+      end
+      user.avatar.attach({ io: File.open(image_path), filename: filename, content_type: mime_type })
+    end
   end
 
   def default_username
