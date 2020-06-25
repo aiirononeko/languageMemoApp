4b908fcafc4e8563ccfbc3eb39f63a097ae90a0e
H37kouya <h37kouya@gmail.com>
2020-06-25 18:27:22 +0900

    refactor & feat 新規ファイル作成とリファクタリング

---
+++ frontend/components/organisms/list/CreateFileListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-list-item>
+    <v-list-item-icon><v-icon>mdi-file</v-icon></v-list-item-icon>
+
+    <v-list-item-content>
+      <v-form @submit.prevent="onSubmit">
+        <v-list-item-title>
+          <v-text-field v-model="model" dense />
+        </v-list-item-title>
+      </v-form>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: undefined
+    }
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.value
+      },
+      set(newVal) {
+        return this.$emit('input', newVal)
+      }
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      return this.$emit('submit')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
