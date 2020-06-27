b9104ab0f415020d111519f11310247ec0fdcc2b
Kanezoh <asfaltmanta114@gmail.com>
2020-06-27 10:29:25 +0900

    feat Omniauthのリダイレクト先をフロントへ

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.full_host = ENV["FRONT_DOMAIN"]
   provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'], scope: 'user, repo', provider_ignores_state: true
   provider :twitter,       ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET'], skip_jwt: true
+  provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET'], skip_jwt: true, callback_path: '/signin'
 end
