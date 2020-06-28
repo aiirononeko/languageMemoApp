dbc3565f625cc62a16140c00b8ee0ce06083191e
Kanezoh <asfaltmanta114@gmail.com>
2020-06-29 00:29:14 +0900

    fix usernameを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name: "ミミたる", email: "mimi@example.com", password: "password", confirmed_at: DateTime.now)
+user = User.create(name: "ミミたる", email: "mimi@example.com", password: "password",
+                   confirmed_at: DateTime.now, username: SecureRandom.alphanumeric(15))
 Post.create(name: "ミミたるポスト", content: "ミミミタルタル", user_id: user.id, folder_id: nil)
 Folder.create(name: "ミミたるフォルダー", user_id: user.id)
