a22334debd0bc2694dd1b7c38df574cd477423eb
Kanezoh <asfaltmanta114@gmail.com>
2020-06-29 00:06:05 +0900

    chore seedファイルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: "ミミたる", email: "mimi@example.com", password: "password")
+Post.create(name: "ミミたるポスト", content: "ミミミタルタル", user_id: user.id, folder_id: nil)
+Folder.create(name: "ミミたるフォルダー", user_id: user.id)
