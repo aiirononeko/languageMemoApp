4b7a1055c11520482177c93478f52cf5fddac2d5
show <show75@ezweb.ne.jp>
2020-07-04 20:40:01 +0900

    feat: SNS認証成功

---
+++ frontend/pages/auth/oauth.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>ログイン中...</h1>
+</template>
+
+<script>
+export default {
+  created() {
+    const { auth_token, client_id, id, uid, username } = this.$route.query
+    const userInfo = { authToken: auth_token, clientId: client_id, id, uid, username }
+    this.$store.dispatch("authentication/loginWithSns", { userInfo })
+    this.$router.push('/settings/profile')
+  }
+}
+</script>
+
+<style>
+
+</style>
