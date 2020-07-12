3321d3001f12ad70e5b32b511f1479762e58f39e
H37kouya <h37kouya@gmail.com>
2020-07-12 21:34:12 +0900

    feat ほかの投稿を見るを表示させた。ユーザー紹介を右にした
    TILのユーザー紹介のカードを右に持っていく。そして、その下にほかの投稿を表示する。

---
+++ frontend/components/organisms/list/OtherPostList.vue
@@ -0,0 +1,67 @@
+<template>
+  <article v-if="postList">
+    <h2 class="main-heading mb-8 text-center">ほかの投稿を見る</h2>
+
+    <v-card v-for="(post, key) in getPostList" :key="key" class="mb-4">
+      <v-card-text>
+        <span class="main-heading card-title-font">{{ post.name }}</span>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer />
+
+        <v-btn
+          :to="getLink(post)"
+          text
+          color="deep-purple accent-4"
+        >
+          投稿を見る
+        </v-btn>
+    </v-card-actions>
+    </v-card>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    userInfo: {
+      type: Object,
+      default: undefined
+    },
+
+    postList: {
+      type: Array,
+      default: undefined
+    }
+  },
+
+  computed: {
+    getLink() {
+      return (post) => {
+        let link = ""
+        for (const ancestorFolder of post.ancestorFolders) {
+          link += `/${ancestorFolder.id}`
+        }
+
+        link += `/${post.uid}`
+        return `/${this.userInfo.username}${link}`
+      }
+    },
+
+    getPostList() {
+      if (!this.postList) {
+        return undefined
+      }
+
+      return this.postList.filter((post) => this.getLink(post) !== this.$route.path)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-title-font {
+  font-size: 1.1rem;
+}
+</style>
