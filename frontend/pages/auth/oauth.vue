004411702a463bc0d816dd05e6423d617b8aa826
show <show75@ezweb.ne.jp>
2020-07-19 10:55:16 +0900

    feat: 初回ログインはuser-nameに遷移するよう変更

--- frontend/pages/auth/oauth.vue
+++ frontend/pages/auth/oauth.vue
@@ -8,7 +8,28 @@ export default {
     const { auth_token, client_id, id, uid, username } = this.$route.query
     const userInfo = { authToken: auth_token, clientId: client_id, id, uid, username }
     this.$store.dispatch("authentication/loginWithSns", { userInfo })
-    this.$router.push('/settings/profile')
+    const redirectPath = this.redirectPath
+    this.setFromSecondTimeLogin()
+    this.$router.push(redirectPath)
+  },
+
+  computed: {
+    redirectPath() {
+        const isFromSecondTimeLogin = window.localStorage.getItem('isFromSecondTimeLogin')
+        return isFromSecondTimeLogin !== 'true' ? '/settings/user-name' : '/settings/profile'
+    }
+  },
+
+  methods: {
+    /**
+     * 2回目以降のログイン判定のために、
+     * ローカルストレージに判定用の値({ 'isFromSecondTimeLogin': 'true' })を入れる
+     */
+    setFromSecondTimeLogin() {
+      if (window.localStorage) {
+        window.localStorage.setItem('isFromSecondTimeLogin', 'true')
+      }
+    }
   }
 }
 </script>
