6a784d21b9f963701b878e599fee06e11721cf03
show <show75@ezweb.ne.jp>
2020-07-23 09:45:02 +0900

    feat: SNSボタンの表示

--- frontend/components/organisms/cards/UserIntroCard.vue
+++ frontend/components/organisms/cards/UserIntroCard.vue
@@ -17,7 +17,7 @@
         </p>
 
         <v-btn v-if="canLookFolderPath" :to="`/${username}`" text color="primary">{{ getName }}のページを見る</v-btn>
-        <UserSnsBtnGroup />
+        <UserSnsBtnGroup :twitterLink="getTwitterLink" :githubLink="getGithubLink" />
       </div>
     </div>
   </v-card>
@@ -51,6 +51,15 @@ export default {
 
     getAvatar() {
       return this.userInfo && this.userInfo.image
+    },
+
+    getTwitterLink() {
+      console.log(this.userInfo)
+      return this.userInfo.twitterLink
+    },
+
+    getGithubLink() {
+      return this.userInfo.githubLink
     },
 
     username() {
