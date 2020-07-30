6bbfa2313cbaa1ba744f7851ee9958df0f13d7d0
Kanezoh <asfaltmanta114@gmail.com>
2020-07-30 15:50:33 +0900

    chore 本番環境のメール送信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,23 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "languageMemoApp_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "languageMemoApp_#{Rails.env}
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_options = {from: 'poeta<no-reply@poeta.work>'}
+  config.action_mailer.default_url_options = { host: "poeta.work"}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :domain => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => "poeta.poetius@gmail.com",
+    :password => ENV["MAIL_PASS"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
