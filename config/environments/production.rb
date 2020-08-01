06ff19ba87dc24d19340cc8acfd5cd0f8d6d11e2
Kanezoh <asfaltmanta114@gmail.com>
2020-08-01 21:26:40 +0900

    chore 本番環境の設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -66,7 +66,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_options = {from: 'poeta<no-reply@poeta.work>'}
-  config.action_mailer.default_url_options = { host: "poeta.work"}
+  #config.action_mailer.default_url_options = { host: "poeta.work"}
+  config.action_mailer.default_url_options = { host: ENV['API_HOST'], port: 3000 }
+
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
